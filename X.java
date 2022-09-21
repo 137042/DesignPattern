d18a757514e6492e3dd8a0f4a4d2a7324b44b317
137042 <mare137042@gmail.com>
2022-09-21 22:40:37 +0900

    [update] overriding, overloading practice

---
+++ X.java
@@ -0,0 +1,18 @@
+class X {
+
+    void methodA(int num) {
+        System.out.println ("methodA:" + num);
+    }
+    void methodA(int num1, int num2) {
+        System.out.println ("methodA:" + num1 + "," + num2);
+    }
+
+    double methodA(double num) {
+        System.out.println("methodA:" + num);
+        return num;
+    }
+
+    public void methodA() { //Base class method
+        System.out.println ("hello, I'm methodA of class X");
+    }
+}
---
+++ Y.java
@@ -0,0 +1,6 @@
+public class Y extends X {
+
+    public void methodA(){
+        System.out.println("hello, I'm methodA of class Y");
+    }
+}
