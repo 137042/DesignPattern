e5956c4dcf5f6e1c5dd9ff3e80c85980d7bfb1e5
137042 <mare137042@gmail.com>
2022-10-13 12:43:40 +0900

    :sparkles:[add] Decorator Pattern practice added

---
+++ decoratorPattern/DarkRoast.java
@@ -0,0 +1,12 @@
+package decoratorPattern;
+
+public class DarkRoast extends Beverage{
+
+    public DarkRoast(){
+        description = "Excellent Dark Roast";
+    }
+
+    public float cost(){
+        return 1.99f + super.cost();
+    }
+}
