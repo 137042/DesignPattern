5653eb5db7be149608723612a0b91b4258deab3c
ChoiHyemin <mare137042@gmail.com>
2022-12-18 04:42:40 +0900

    :sparkles:[update] state/proxy-protection/virtual pattern practice, uml added :rocket:

---
+++ hf/dp/state/GumballMachineTestDrive.java
@@ -0,0 +1,50 @@
+package hf.dp.state;
+
+import hf.MyHWInfo;
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		MyHWInfo.printInfo();
+
+		GumballMachine gumballMachine = 
+			new GumballMachine(10);
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+
+		System.out.println(gumballMachine);
+	}
+}
